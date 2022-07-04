e82314666528ad4a8f08687944f309b720d0b1f1
younheesong <thd6517@naver.com>
2022-07-05 01:33:10 +0900

    feat: member model add

---
+++ models/meber/member.model.ts
@@ -0,0 +1,44 @@
+import { InAuthUser } from '../in_auth_user';
+import FirebaseAdmin from '../firebase_admin';
+
+const MEMBER_COL = 'members';
+const SCR_NAME_COL = 'screen_names';
+
+type AddResult = { result: true; id: string } | { result: false; message: string };
+
+async function add({ uid, email, displayName, photoURL }: InAuthUser): Promise<AddResult> {
+  try {
+    const screenName = (email as string).replace('@gmail.com', '');
+    const addResult = await FirebaseAdmin.getInstance().Firebase.runTransaction(async (transaction) => {
+      const memberRef = FirebaseAdmin.getInstance().Firebase.collection(MEMBER_COL).doc(uid);
+      const screenNameRef = FirebaseAdmin.getInstance().Firebase.collection(SCR_NAME_COL).doc(screenName);
+      const memberDoc = await transaction.get(memberRef);
+      if (memberDoc.exists) {
+        // 이미 추가된 상태
+        return false;
+      }
+      const addData = {
+        uid,
+        email,
+        displayName: displayName ?? '',
+        photoURL: photoURL ?? '',
+      };
+      await transaction.set(memberRef, addData);
+      await transaction.set(screenNameRef, addData);
+      return true;
+    });
+    if (addResult === false) {
+      return { result: true, id: uid };
+    }
+    return { result: true, id: uid };
+  } catch (e) {
+    console.error(e);
+    return { result: false, message: 'server error' };
+  }
+}
+
+const MemberModel = {
+  add,
+};
+
+export default MemberModel;
--- pages/api/members.add.ts
+++ pages/api/members.add.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 import { NextApiRequest, NextApiResponse } from 'next';
-import FirebaseAdmin from '@/models/firebase_admin';
+import MemberModel from '@/models/meber/member.model';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { uid, displayName, photoURL, email } = req.body;
@@ -11,51 +11,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   if (email === undefined || email === null) {
     res.status(400).json({ result: false, message: 'email 누락됬습니다.' });
   }
-  try {
-    // const addResult = await FirebaseAdmin.getInstance()
-    //   .Firebase.collection('members')
-    //   .doc(uid)
-    //   .set({
-    //     uid,
-    //     email: email ?? '',
-    //     displayName: displayName ?? '',
-    //     photoURL: photoURL ?? '',
-    //   });
-    // const screenName = email.replace('@gmail.com', '');
-    // await FirebaseAdmin.getInstance()
-    //   .Firebase.collection('screen_names')
-    //   .doc(screenName)
-    //   .set({
-    //     uid,
-    //     email,
-    //     displayName: displayName ?? '',
-    //     photoURL: photoURL ?? '',
-    //   });
-    const screenName = email.replace('@gmail.com', '');
-    const addResult = await FirebaseAdmin.getInstance().Firebase.runTransaction(async (transaction) => {
-      const memberRef = FirebaseAdmin.getInstance().Firebase.collection('members').doc(uid);
-      const screenNameRef = FirebaseAdmin.getInstance().Firebase.collection('screen_names').doc(screenName);
-      const memberDoc = await transaction.get(memberRef);
-      if (memberDoc.exists) {
-        // 이미 추가된 상태
-        return false;
-      }
-      const addData = {
-        uid,
-        email,
-        displayName: displayName ?? '',
-        photoURL: photoURL ?? '',
-      };
-      await transaction.set(memberRef, addData);
-      await transaction.set(screenNameRef, addData);
-      return true;
-    });
-    if (addResult === false) {
-      return res.status(200).json({ result: true });
-    }
-    return res.status(200).json({ result: true, id: uid });
-  } catch (e) {
-    console.error(e);
-    return res.status(500).json({ result: false });
+  const addResult = await MemberModel.add({ uid, email, displayName, photoURL });
+  if (addResult.result === true) {
+    return res.status(200).json(addResult);
   }
+  return res.status(500).json(addResult);
 }
