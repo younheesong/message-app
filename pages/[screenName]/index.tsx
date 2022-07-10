0eb9c2b6a7a4755ba4fb21d144058e0b08ae21c1
younheesong <thd6517@naver.com>
2022-07-11 00:28:36 +0900

    feat: input 대화 기능 design

--- pages/[screenName]/index.tsx
+++ pages/[screenName]/index.tsx
@@ -1,7 +1,8 @@
-import { Avatar, Box, Flex, Text } from '@chakra-ui/react';
+import { Avatar, Box, Button, Flex, Text, Textarea, useToast } from '@chakra-ui/react';
 import { NextPage } from 'next';
+import ResizeTextArea from 'react-textarea-autosize';
+import { useState } from 'react';
 import { ServiceLayout } from '@/components/service_layout';
-// import { UserInfo } from 'fiebase-admin/lib/auth/user-record';
 
 const userInfo = {
   uid: 'test',
@@ -11,6 +12,8 @@ const userInfo = {
 };
 
 const UserHomePage: NextPage = function () {
+  const [message, setMessage] = useState('');
+  const toast = useToast();
   return (
     <ServiceLayout title="user home" minH="100vh" backgroundColor="gray.200">
       <Box maxW="md" mx="auto" pt="6">
@@ -21,6 +24,40 @@ const UserHomePage: NextPage = function () {
               <Text fontSize="md">{userInfo.displayName}</Text>
               <Text fontSize="md">{userInfo.email}</Text>
             </Flex>
+          </Flex>
+        </Box>
+        <Box borderWidth="1px" borderRadius="lg" overflow="hidden" mb="2" bg="white">
+          <Flex align="center" p="2">
+            <Avatar size="xs" mr="2" src="https://bit.ly/broken-link" />
+            <Textarea
+              bg="gray.100"
+              border="none"
+              placeholder="무엇이 궁금한가요?"
+              resize="none"
+              minH="unset"
+              overflow="hidden"
+              fontSize="xs"
+              mr="2"
+              as={ResizeTextArea}
+              maxRows={7}
+              value={message}
+              onChange={(e) => {
+                if (e.currentTarget.value) {
+                  const lineCount = (e.currentTarget.value.match(/[^\n]*\n]*/gi)?.length ?? 1) + 1;
+                  if (lineCount > 7) {
+                    toast({
+                      title: '최대 7줄까지 입력 가능합니다.',
+                      position: 'top-right',
+                    });
+                    return;
+                  }
+                }
+                setMessage(e.currentTarget.value);
+              }}
+            />
+            <Button disabled={message.length === 0} bgColor="#ff886c" color="white" colorScheme="yellow" size="sm">
+              등록
+            </Button>
           </Flex>
         </Box>
       </Box>
